6bc63374eeab75b1a1ed561153b2231fb983cac4
Rohitverma-creator <uttamv747@example.com>
2026-02-16 14:19:19 +0530

    Added Music Player Desgin

---
+++ Day13/models/Song.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include<iostream>
+#include<string>
+
+using namespace std;
+class Song{
+    private:
+    string title;
+    string artist;
+    string filePath;
+    public:
+    Song(string title, string artist,string filePath){
+        this->title=title;
+        this->artist=artist;
+        this->filePath=filePath;
+    }
+   string getTitle(){
+    return title;
+   }
+   string getArtist(){
+    return artist;
+   }
+   string getFilePath(){
+    return filePath;
+   }
+};
